65eb38ee4e6138cf68536a0be2fed72aa6a60ca8
@hrahmat950 <hrahmat950@gmail.com>
2023-06-08 18:52:44 +0700

    add server

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ const { parse } = require("url");
 const next = require("next");
 const makanan = require("./pages/data/dataMakanan");
 
-// const dev = process.env.NODE_ENV !== "production";
-// const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3000;
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  const server = createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     const { pathname } = parsedUrl;
 
-    if (pathname === "/a" || pathname === "/b") {
-      app.render(req, res, pathname, parsedUrl.query);
+    if (pathname === "/") {
+      // Tambahkan log atau pernyataan lain untuk memastikan penanganan halaman utama tercapai
+      console.log('Halaman utama ditangani');
+
+      // Tambahkan kode untuk menampilkan halaman utama
+      app.render(req, res, "/", parsedUrl.query);
     } else if (pathname === "/api/makanan") {
       res.setHeader("Content-Type", "application/json");
       res.statusCode = 200;
@@ -23,9 +27,10 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl);
     }
-  }).listen(port, (err) => {
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
     console.log(`Server berjalan pada http://localhost:${port}`);
   });
 });
-module.exports = server.js;
