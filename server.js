f1785c8e5a42cbbd0adc49cffc188ae29c402a11
@hrahmat950 <hrahmat950@gmail.com>
2023-06-08 18:30:29 +0700

    config server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const makanan = require('./pages/data/dataMakanan');
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+const makanan = require("./pages/data/dataMakanan");
 
-const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 3000;
+// const dev = process.env.NODE_ENV !== "production";
+// const port = process.env.PORT || 3000;
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -14,10 +14,10 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url, true);
     const { pathname } = parsedUrl;
 
-    if (pathname === '/a' || pathname === '/b') {
+    if (pathname === "/a" || pathname === "/b") {
       app.render(req, res, pathname, parsedUrl.query);
-    } else if (pathname === '/api/makanan') {
-      res.setHeader('Content-Type', 'application/json');
+    } else if (pathname === "/api/makanan") {
+      res.setHeader("Content-Type", "application/json");
       res.statusCode = 200;
       res.end(JSON.stringify(makanan));
     } else {
@@ -28,3 +28,4 @@ app.prepare().then(() => {
     console.log(`Server berjalan pada http://localhost:${port}`);
   });
 });
+module.exports = server.js;
